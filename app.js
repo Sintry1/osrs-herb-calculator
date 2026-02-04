e32790e16c28d73fd6b10d9f964a9db2b21d5e49
Sintry1 <75076281+Sintry1@users.noreply.github.com>
2026-02-04 15:53:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ function setLoading(isLoading) {
 // Register Service Worker for PWA
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(registration => {
                 console.log('ServiceWorker registered:', registration);
             })
