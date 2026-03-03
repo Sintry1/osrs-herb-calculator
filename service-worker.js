87cdbb6b3529474737617c74517860f7cf06e6ab
Dominic Smith <ds@weblager.dk>
2026-03-03 12:46:37 +0100

    version bump

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker for OSRS Herb Profit Calculator
 // Enables offline functionality and caching
 
-const CACHE_NAME = 'osrs-herb-calc-v1';
+const CACHE_NAME = 'osrs-herb-calc-v2';
 const urlsToCache = [
     '/',
     '/index.html',
